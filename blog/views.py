606355a5cb09f2d6318a5f5dd4479d46feab275d
alexis <alexisverardo95@gmail.com>
2017-06-14 00:26:27 -0300

    casi listo

--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,14 @@ from .forms import PostForm
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html' , {'post': post})
+
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts' : posts})
+
 
 def post_new(request):
     if request.method == "POST":
@@ -17,14 +22,15 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect(post_detail, pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+        return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_detail(request, pk):
-		post = get_object_or_404(Post, pk=pk)
-		return render(request, 'blog/post_detail.html' , {'post': post})
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html' , {'post': post})    
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -33,7 +39,7 @@ def post_edit(request, pk):
             post = form.save(commit=False)
             post.author = request.user
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect(post_detail, pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+        return render(request, 'blog/post_edit.html', {'form': form})
